d0fa6a66e786a3f6364a224171fd4a8a8eb23097
Jonas Mumm <jonasmumm@web.de>
2025-11-11 11:58:42 +0100

    add hit sound

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody playerRigidbody;
     public InputActionReference inputAction;
     public float playerAcceleration;
+    public AudioSource hitAudio;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,6 +27,7 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Obstacle"))
         {
+            hitAudio.Play();
             SceneManager.LoadScene(0);
         }
     }
